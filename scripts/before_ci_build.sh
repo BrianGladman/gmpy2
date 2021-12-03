7015c1c3a5e6948763e65a826829690219cd56fb
Dima Pasechnik <dima@pasechnik.info>
2021-12-03 18:39:45 +0000

    update and simplify linux/macOS wheels job

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -1,16 +1,22 @@
+ver_gmp=6.2.0
+ver_mpfr=4.1.0
+ver_mpc=1.2.1
 if [ ! -f finish_before_ci_build ]; then
   if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    yum install -y wget lzip
-    wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.lz
-    tar -xvf gmp-6.2.0.tar.lz
-    cd gmp-6.2.0 && ./configure && make --enable-fat -j4 && make install && cd ../
-    wget https://www.mpfr.org/mpfr-current/mpfr-4.0.2.tar.gz
-    tar -xvf mpfr-4.0.2.tar.gz
-    cd mpfr-4.0.2 && ./configure && make -j4 && make install && cd ../
-    wget https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
-    tar -xvf mpc-1.1.0.tar.gz
-    cd mpc-1.1.0 && ./configure && make -j4 && make install && cd ../
+    curl -O https://gmplib.org/download/gmp/gmp-$ver_gmp.tar.lz
+    tar -xf gmp-$ver_gmp.tar.lz
+    cd gmp-$ver_gmp && ./configure && make --enable-fat -j4 install && cd ../
+
+    curl -O https://www.mpfr.org/mpfr-current/mpfr-$ver_mpfr.tar.gz
+    tar -xf mpfr-$ver_mpfr.tar.gz
+    cd mpfr-$ver_mpfr && ./configure && make -j4 install && cd ../
+
+    curl -O https://ftp.gnu.org/gnu/mpc/mpc-$ver_mpc.tar.gz
+    tar -xf mpc-$ver_mpc.tar.gz
+    cd mpc-$ver_mpc && ./configure && make -j4 install && cd ../
+
     pip install Cython
+
   elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install gmp mpfr libmpc
   fi
